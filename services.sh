c2a1737f3ce674775f0cbf6cd64835b750bca790
Kauã Onorato <kauaonorato.dev@gmail.com>
2024-11-22 20:04:45 -0400

    update gcloud config

--- services.sh
+++ services.sh
@@ -1,3 +1,4 @@
+gcloud config set project solar-botany-442600-s9
 gcloud services enable run.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
 gcloud services enable artifactregistry.googleapis.com
